1072ef7943773c0c0d738bd1f6181ade5fa07c9c
Niumiueb <n.ebrahiminoor@gmail.com>
2025-07-10 13:41:38 -0400

    need to access the ::after

---
+++ js.js
@@ -0,0 +1,7 @@
+// const elements = document.querySelectorAll(".sport")
+// elements.forEach((element)=>{
+//     element.addEventListener("mouseover",()=>{
+//         element.style.color = 'transparetn';
+//         element.after.style.display = 'none';
+//     })
+// })
