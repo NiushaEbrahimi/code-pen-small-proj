4ac4d8973253232f24b51109cc9fa85a3025c87c
Niumiueb <n.ebrahiminoor@gmail.com>
2025-07-13 11:30:45 -0400

    need to fix the navbar

--- js.js
+++ js.js
@@ -1,7 +1,15 @@
-// const elements = document.querySelectorAll(".sport")
-// elements.forEach((element)=>{
-//     element.addEventListener("mouseover",()=>{
-//         element.style.color = 'transparetn';
-//         element.after.style.display = 'none';
-//     })
-// })
+const homeDefualt = document.querySelector(".home-icon")
+homeDefualt.style.color='black';
+homeDefualt.style.background ='white';
+const elements = document.querySelectorAll(".icon")
+elements.forEach((element)=>{
+    element.addEventListener("click",()=>{
+        elements.forEach((element)=>{
+            if(element.style.color==='black'){
+                element.style.color='white';
+                element.style.background ='transparent';
+            }})
+        element.style.color = 'black';
+        element.style.background ='white';
+    })
+})
