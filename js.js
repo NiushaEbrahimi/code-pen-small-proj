f141e7535fee52d323580a6949b87a83ed7031f9
Niumiueb <n.ebrahiminoor@gmail.com>
2025-07-15 14:37:45 -0400

    finishing touches

--- js.js
+++ js.js
@@ -1,15 +1,11 @@
-const homeDefualt = document.querySelector(".home-icon")
-homeDefualt.style.color='black';
-homeDefualt.style.background ='white';
-const elements = document.querySelectorAll(".icon")
+const elements = document.querySelectorAll(".nav-item")
 elements.forEach((element)=>{
     element.addEventListener("click",()=>{
         elements.forEach((element)=>{
-            if(element.style.color==='black'){
-                element.style.color='white';
-                element.style.background ='transparent';
-            }})
-        element.style.color = 'black';
-        element.style.background ='white';
+            if(element.classList.contains('active')){
+                element.classList.remove('active');
+            }
+        });
+        element.classList.add('active');
     })
 })
